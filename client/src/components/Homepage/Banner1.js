16cc6ea5b2631260e43b52ac17b40fb00487fa84
macuser <macuser@MacBook-Pro.local>
2023-04-22 13:52:43 -0700

    added contact number to banner

--- client/src/components/Homepage/Banner1.js
+++ client/src/components/Homepage/Banner1.js
@@ -4,13 +4,12 @@ import styles from "./Banner1.module.css";
 import { selectTSML } from "../../utils";
 import ExpandButton from "../GeneralUI/ExpandButton";
 
-import bg_mobile from "../../images/solarspan1_mobile.jpg";
 import bg_medium from "../../images/solarspan10.jpg";
 import nabcepSeal from "../../images/pv_assoc_seal.png";
 
 const Banner1 = (props) => {
   const w = props.viewportWidth;
-  const background = selectTSML(w, bg_medium, bg_mobile, bg_medium, bg_medium);
+  const background = selectTSML(w, bg_medium, bg_medium, bg_medium, bg_medium);
   const minHeight = selectTSML(w, "100vh", "28rem", "28rem", "46rem");
   const fontSize = selectTSML(w, "1.5rem", "2rem", "2.2rem", "");
   const nebcepJustify = selectTSML(w, "center", "end", "", "");
@@ -34,7 +33,22 @@ const Banner1 = (props) => {
     "fixed"
   );
   const textMargin = selectTSML(w, "7rem 1rem 0 1rem");
-  const expandButtonMarginBottom = selectTSML(w, "0rem", "-4.7rem", "-4.7rem", "-4.7rem")
+  const expandButtonMarginBottom = selectTSML(
+    w,
+    "0rem",
+    "-4.7rem",
+    "-4.7rem",
+    "-4.7rem"
+  );
+  const phoneNumberFontSize = selectTSML(w, "1rem", "2rem", "2.4rem", "2.4rem");
+  const phoneNumberMargin = selectTSML(
+    w,
+    "1rem 0 0 1rem",
+    "1rem 2.7rem 0 0",
+    "1rem 2.7rem 0 0",
+    "1rem 2.7rem 0 0"
+  );
+  const phoneNumberTextAlign = selectTSML(w, "left");
 
   return (
     <div style={{ zoom: props.zoom }}>
@@ -62,6 +76,16 @@ const Banner1 = (props) => {
                 Off-grid and grid-tied systems<br></br>
               </div>
               <div
+                style={{
+                  fontSize: phoneNumberFontSize,
+                  margin: phoneNumberMargin,
+                  textAlign: phoneNumberTextAlign,
+                }}
+              >
+                604.723.5351
+              </div>
+
+              <div
                 className={styles.nabcepContainer}
                 style={{
                   display: nabcepDisplay,
@@ -76,7 +100,10 @@ const Banner1 = (props) => {
                 />
               </div>
             </div>
-            <div className={styles.expandButtonContainer} style={{marginBottom: expandButtonMarginBottom}}>
+            <div
+              className={styles.expandButtonContainer}
+              style={{ marginBottom: expandButtonMarginBottom }}
+            >
               <ExpandButton
                 style={{
                   marginBottom: expandButtonMargin,
