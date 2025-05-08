b0a65d7df673ae35819aa72e70e879ee28509778
Dev <dev@MacBook-Pro.local>
2025-05-08 07:08:32 -0700

    updated testimonials to include shaun and updated testimonials size

--- client/src/components/Homepage/Testimonials.jsx
+++ client/src/components/Homepage/Testimonials.jsx
@@ -17,25 +17,25 @@ const testimonials = [
     image: cathcart,
     header: "My husband and I were delighted with the service",
     text: "and we love our solar panels! From Helion's response to our initial inquiry through the city and BC Hydro application processes and finishing with the installation of our solar panels, we found Helion to be extremely responsive, thorough and professional. They took the time to explain which system would work best for our needs and to suggest solutions for the installation of the solar panels on our roof and the EV charger we required. The workmanship was excellent. ",
-    source: "S. Cathcart (11.34 kW Grid-Tied System 2025)",
+    source: "Shaun C. (11.34 kW Grid-Tied System 2025)",
   },
   {
     image: chad,
     header: "I highly recommend this company.",
     text: "They were very professional and easy to work with. From the beginning to the end of the project, they effortlessly helped navigate through the grant & loan process answering every question I had along the way. The finished product looked amazing!!",
-    source: "C. Peters (14.11 kW Grid-Tied System 2024)",
+    source: "Chad P. (14.11 kW Grid-Tied System 2024)",
   },
   {
     image: parminder,
     header: "Helion Solar did a great job with our install,",
     text: "we are very happy to recommend them as solar installers. They were able to include our electrical upgrades in the price and complete them along with the solar installation. From start to finish the job was very professionally done.",
-    source: "P. Raikhi (10.01kW Grid-Tied System 2023)",
+    source: "Parminder R. (10.01kW Grid-Tied System 2023)",
   },
   {
     image: marja,
     header: "Our off-grid install was done quickly and efficiently",
     text: "and Helion Solar was able to address various issues and surprises that arose due to the remote location and/or the age of the cabin and complete the installation without delays.  We have been very happy with the system and it has provided us much needed electricity that has made our cabin experience much more enjoyable. I warmly recommend Helion Solar.",
-    source: "M. Rihijarvi (790kW Off-Grid System 2022)",
+    source: "Marja R. (790kW Off-Grid System 2022)",
   }
 ];
 
